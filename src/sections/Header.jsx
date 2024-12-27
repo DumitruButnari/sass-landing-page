823504fa08fecac60cd89ca5da724f4425c81282
DumitruButnari <dimkalanu@gmail.com>
2024-12-27 13:57:44 +0200

    finish features section. Fixed header scolling issue

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,9 +1,22 @@
 import { Link as LinkScroll } from "react-scroll";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import clsx from "clsx";
+import PropTypes from "prop-types";
 
 const Header = () => {
+  const [hasScrolled, setHasScrolled] = useState(false);
   const [isOpenMenu, setIsOpenMenu] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setHasScrolled(window.scrollY > 32);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   const toggleMenu = () => {
     setIsOpenMenu(!isOpenMenu);
   };
@@ -24,8 +37,17 @@ const Header = () => {
     );
   }
 
+  NavLink.propTypes = {
+    title: PropTypes.string,
+  };
+
   return (
-    <header className={"fixed top-0 left-0 z-50 w-full py-10"}>
+    <header
+      className={clsx(
+        "fixed top-0 left-0 z-50 w-full py-10 transition-all duration-500 max-lg:py-4",
+        hasScrolled && "py-2 bg-black-100 backdrop-blur-[8px]",
+      )}
+    >
       <div className={"container flex h-14 items-center max-lg:px-5"}>
         <a className={"lg:hidden flex-1 cursor-pointer z-2"}>
           <img src="/images/xora.svg" width={115} height={55} alt="logo" />
@@ -54,7 +76,7 @@ const Header = () => {
                 <li className={"nav-logo"}>
                   <LinkScroll
                     to={"hero"}
-                    offset={-100}
+                    offset={-250}
                     spy
                     smooth
                     className={clsx(
@@ -69,6 +91,7 @@ const Header = () => {
                     />
                   </LinkScroll>
                 </li>
+
                 <li className={"nav-li"}>
                   <NavLink title={"faq"} />
                   <div className={"dot"} />
@@ -76,6 +99,7 @@ const Header = () => {
                 </li>
               </ul>
             </nav>
+
             <div
               className={
                 "lg:hidden block absolute top-1/2 left-0 w-[960px] h-[380px] translate-x-[-290px] -translate-y-1/2 rotate-90"
